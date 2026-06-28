78db74cf2c7bb8301c9de81ae9492c1e9af47354
sheezan-yo <khansheezan30@gmail.com>
2026-06-28 23:43:47 +0530

    env to frontend

--- frontend/src/services/taskApi.js
+++ frontend/src/services/taskApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/tasks",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/tasks",
 });
 
 export const getTasks = () => API.get("/");
